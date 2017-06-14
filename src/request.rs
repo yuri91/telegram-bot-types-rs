e39531760c4760ff0ae91fa5b63a8a6c1e974d7d
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-06-14 22:37:23 +0200

    Add type safe parse_mode field to message

--- src/request.rs
+++ src/request.rs
@@ -1,6 +1,39 @@
-#[derive(Clone, Debug, Serialize)]
+use std::default::Default;
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize)]
+pub enum ParseMode {
+    Text,
+    Markdown,
+    Html
+}
+impl ParseMode {
+    fn is_text(&self) -> bool {
+        *self == ParseMode::Text
+    }
+}
+impl Default for ParseMode {
+    fn default() -> ParseMode {
+        ParseMode::Text
+    }
+}
+#[derive(Clone, Debug, Serialize, Default)]
 pub struct Message {
     pub chat_id: i64,
     pub text: String,
+    #[serde(skip_serializing_if="ParseMode::is_text")]
+    pub parse_mode: ParseMode,
     //TODO
+}
+impl Message {
+    pub fn new(chat_id: i64, text: String) -> Message {
+        Message {
+            chat_id,
+            text,
+            ..Default::default()
+        }
+    }
+    pub fn parse_mode(mut self, mode: ParseMode) -> Message {
+        self.parse_mode = mode;
+        self
+    }
 }
