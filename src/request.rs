cccda3b36ddb739f0270840a322f71fcccd1deb8
Yuri Iozzelli <y.iozzelli@gmail.com>
2017-08-01 00:53:48 +0200

    Basic and partial support for inline query messages

--- src/request.rs
+++ src/request.rs
@@ -4,7 +4,7 @@ use std::default::Default;
 pub enum ParseMode {
     Text,
     Markdown,
-    Html
+    Html,
 }
 impl ParseMode {
     fn is_text(&self) -> bool {
@@ -20,7 +20,7 @@ impl Default for ParseMode {
 pub struct Message {
     pub chat_id: i64,
     pub text: String,
-    #[serde(skip_serializing_if="ParseMode::is_text")]
+    #[serde(skip_serializing_if = "ParseMode::is_text")]
     pub parse_mode: ParseMode,
     //TODO
 }
@@ -35,5 +35,51 @@ impl Message {
     pub fn parse_mode(mut self, mode: ParseMode) -> Message {
         self.parse_mode = mode;
         self
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Default)]
+pub struct AnswerInlineQuery {
+    pub inline_query_id: String,
+    pub results: Vec<InlineQueryResult>,
+    pub cache_time: Option<i64>,
+    pub is_personal: Option<bool>,
+    pub next_offset: String,
+    pub switch_pm_text: Option<String>,
+    pub switch_pm_parameter: Option<String>,
+}
+impl AnswerInlineQuery {
+    pub fn new(
+        inline_query_id: String,
+        results: Vec<InlineQueryResult>,
+        next_offset: String,
+    ) -> AnswerInlineQuery {
+        AnswerInlineQuery {
+            inline_query_id,
+            results,
+            next_offset,
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "lowercase")]
+pub enum InlineQueryResult {
+    Article {
+        id: String,
+        title: String,
+        message_text: String,
+        //TODO
+    },
+}
+impl InlineQueryResult {
+    pub fn article(id: String, title: String, message_text: String) -> InlineQueryResult {
+        InlineQueryResult::Article {
+            id,
+            title,
+            message_text,
+        }
     }
 }
